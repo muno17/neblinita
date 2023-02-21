45ff3d5c98f453977cb995da73279d02fec166c0
Daniel Munoz <munozd17@gmail.com>
2023-02-21 12:29:49 -0500

    initial commit

--- chorus.py
+++ chorus.py
@@ -11,6 +11,24 @@ s.boot()
 interface = Input().play().out()
 
 
+# Mix the source in stereo and send the signal to the output.
+interface_out = interface.mix(2).out()
+
+# Sets values for 8 LFO'ed delay lines (you can add more if you want!).
+# LFO frequencies.
+freqs = [0.254, 0.465, 0.657, 0.879, 1.23, 1.342, 1.654, 1.879]
+# Center delays in seconds.
+cdelay = [0.0087, 0.0102, 0.0111, 0.01254, 0.0134, 0.01501, 0.01707, 0.0178]
+# Modulation depths in seconds.
+adelay = [0.001, 0.0012, 0.0013, 0.0014, 0.0015, 0.0016, 0.002, 0.0023]
+
+# Create 8 sinusoidal LFOs with center delays "cdelay" and depths "adelay".
+lfos = Sine(freqs, mul=adelay, add=cdelay)
+
+# Create 8 modulated delay lines with a little feedback and send the signals
+# to the output. Streams 1, 3, 5, 7 to the left and streams 2, 4, 6, 8 to the
+# right (default behaviour of the out() method).
+delays = Delay(interface, lfos, feedback=0.5, mul=0.5).out()
 
 
 s.gui(locals())
