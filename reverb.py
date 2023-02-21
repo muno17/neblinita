4ef2d8ac6e284f6e4efd5434620564f7fa239c26
Daniel Munoz <munozd17@gmail.com>
2023-02-21 12:02:34 -0500

    initial commit

--- reverb.py
+++ reverb.py
@@ -11,6 +11,23 @@ s.boot()
 interface = Input().play().out()
 
 
+# Four parallel stereo comb filters. The delay times are chosen
+# to be as uncorrelated as possible. Prime numbers are a good
+# choice for delay lengths in samples.
+comb1 = Delay(interface, delay=[0.0997, 0.4277], feedback=0.65)
+comb2 = Delay(interface, delay=[0.7371, 0.0393], feedback=0.51)
+comb3 = Delay(interface, delay=[0.5411, 0.0409], feedback=0.5)
+comb4 = Delay(interface, delay=[0.1137, 0.7155], feedback=0.73)
+
+combsum = interface + comb1 + comb2 + comb3 + comb4
+
+# The sum of the original signal and the comb filters
+# feeds two serial allpass filters.
+all1 = Allpass(combsum, delay=[0.005, 0.00507], feedback=0.75)
+all2 = Allpass(all1, delay=[0.0117, 0.0123], feedback=0.61)
+
+# Brightness control.
+lowp = Tone(all2, freq=3500, mul=0.25).out()
 
 
 
