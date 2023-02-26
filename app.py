835fa614eb6c7435bd2e2e593583abf16a12c8c1
Daniel Munoz <munozd17@gmail.com>
2023-02-25 23:52:40 -0500

    initial commit

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6.QtGui import QPalette, QColor
 
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+        
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
