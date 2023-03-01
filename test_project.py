543145d42b7841001282bacb7860da75b6abab64
Daniel Munoz <munozd17@gmail.com>
2023-03-01 18:23:31 -0500

    testing

--- test_project.py
+++ test_project.py
@@ -1,6 +1,26 @@
+from project import 
+from project import
+from project import
 
+
+
+def test_function_1():
+    ...
+
+
+def test_function_2():
+    ...
+
+
+def test_function_n():
+    ...
 
 """
 - don't allow a feedback loop to be created by the computer
     - input of microphone and output of built in speaker not allowed
+"""
+
+"""
+- don't allow "zsh: trace trap  /opt/homebrew/bin/python3 gui3.py"
+
 """
