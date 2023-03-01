02148a0d05b8437c771df978bb228d513d255332
Daniel Munoz <munozd17@gmail.com>
2023-03-01 00:22:39 -0500

    gui testing

--- pysimp.py
+++ pysimp.py
@@ -1,41 +1,16 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('neblina'), sg.Text('', key='-OUTPUT-')],
-          [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('wet/dry'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('melt'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('fractals'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('luz delay'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('luz space'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('haze'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('sombra delay'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
-           [sg.Text('sombra space'), sg.Text('', key='-OUTPUT-')],
-           [sg.T('0',size=(4,1), key='-LEFT-'),
-           sg.Slider((0,100), key='-SLIDER-', orientation='h', enable_events=True, disable_number_display=True),
-           sg.T('0', size=(4,1), key='-RIGHT-')],
+
+layout = [[sg.Text('neblina', font='Monaco'), sg.Text('', key='-OUTPUT-')],
+          [sg.Combo(('input1', 'input2', 'input3')), sg.Combo(('output1', 'output2', 'output3'))],
+          [sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco'),
+           sg.Slider((0,100), key='-SLIDER-', orientation='v', tick_interval=50, enable_events=True, disable_number_display=True, font='Monaco')],
           [sg.Button('Show'), sg.Button('Exit')]]
 
 window = sg.Window('neblina', layout)
@@ -45,8 +20,8 @@ while True:             # Event Loop
     #print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    window['-LEFT-'].update(int(values['-SLIDER-']))
-    window['-RIGHT-'].update(int(values['-SLIDER-']))
+    #window['-LEFT-'].update(int(values['-SLIDER-']))
+    #window['-RIGHT-'].update(int(values['-SLIDER-']))
     if event == 'Show':
         sg.popup(f'The slider value = {values["-SLIDER-"]}')
 window.close()
