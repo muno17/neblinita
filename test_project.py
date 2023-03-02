45e2e945faa9b283582af21e0f826f587f9f36ea
Daniel Munoz <munozd17@gmail.com>
2023-03-01 19:49:39 -0500

    testing

--- test_project.py
+++ test_project.py
@@ -1,25 +1,18 @@
+import pytest
 from project import bufculator
-#from project import
-#from project import
+from project import distortion
+from project import delay1
 
 
 def test_bufculator():
     assert bufculator(256, 44100)  == 0.005804988662131519
 
 
-def test_function_2():
-    ...
+def test_distortion():
+    with pytest.raises(TypeError):
+        distortion()
 
 
-def test_function_n():
-    ...
-
-"""
-- don't allow a feedback loop to be created by the computer
-    - input of microphone and output of built in speaker not allowed
-"""
-
-"""
-- don't allow "zsh: trace trap  /opt/homebrew/bin/python3 gui3.py"
-
-"""
+def test_delay1():
+    with pytest.raises(TypeError):
+        delay1()
